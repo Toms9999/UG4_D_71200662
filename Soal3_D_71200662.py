39664ecf5bc910054cfc6be6c591f20f4dd849c8
Thomas Janir <117893395+Toms9999@users.noreply.github.com>
2023-03-01 22:14:01 +0800

    Update Soal3_D_71200662.py

--- Soal3_D_71200662.py
+++ Soal3_D_71200662.py
@@ -1,16 +1,13 @@
 def cari_kata(kalimat):
-    kata = kalimat.split
-    kataterpanjang = [0]
+    kata = kalimat.split()
 
-for k in kalimat:
-    if len(k) > len(kataterpanjang):
-        kataterpanjang = k
-        return kataterpanjang
+    kataterpanjang = kata[0]
+    for k in kata:
+        if len(k) > len(kataterpanjang):
+            kataterpanjang = k
+    return kataterpanjang
 
 kalimat = input('Masukkan sebuah Kalimat : ')
-
+kataterpanjang = cari_kata(kalimat) 
 
 print('kata terpanjang dalam kalimat tersebut adalah : ', kataterpanjang)    
-
-    
-    
